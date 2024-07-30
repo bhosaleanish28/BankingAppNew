e05dcc43394df59d81490e8fe1ea2d3fcae50a89
Anish.Bhosale <Anish.Bhosale@DELL1527>
2024-07-30 14:04:52 +0530

    Add project files.

---
+++ BankingAppNew/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountManager;
+
+namespace BankingAppNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.accounts = SerialDeserial.DeserializeData();
+            Class1.BankingStart();
+            SerialDeserial.SerializeData(Class1.accounts);
+        }
+    }
+
+}
